7c3da124b790563335d07429fd2e141ae0b5cf18
Maxim Baykevich <max.baikevich@gmail.com>
2020-03-19 19:23:05 +0300

    add del element

--- src/components/todo-list/todo-list.js
+++ src/components/todo-list/todo-list.js
@@ -3,13 +3,15 @@ import React from "react";
 import TodoListItem from "../todo-list-item";
 import "./todo-list.css";
 
-const TodoList = ({ todos }) => {
+const TodoList = ({ todos, onDelited }) => {
   const elements = todos.map(item => {
     const { id, ...itemProps } = item;
 
     return (
       <li key={id} className="list-group-item">
-        <TodoListItem {...itemProps} />
+        <TodoListItem {...itemProps} 
+        onDelited = {()=> onDelited(id)}
+        />
       </li>
     );
   });
